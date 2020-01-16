e2b30806e327630340362470bec0c814448bdf39
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2020-01-16 13:06:16 +0530

    Initial Work

---
+++ Source/Modules/Resources/views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php
+      include '../bootstrap/bootstrap4_header.php';
+    ?>    
+    <title>eLibrary</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+    <?php
+      include '../bootstrap/bootstrap4_footer.php';
+    ?>
+  </body>
+</html>
