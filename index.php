80c690f4020847b4817a5ded4cdcaba7d23b1130
Gauravbhatt19 <gauravbhatt1924@gmail.com>
2020-01-29 17:05:35 +0530

    MD formatting in Readme

--- index.php
+++ index.php
@@ -8,12 +8,10 @@
 </head>
 <body>
 	<?php  
-	
 	require 'core/bootstrap.php';
 	$loginURL = $gClient->createAuthUrl();
 	require Router::load('routes.php')->direct(Request::uri());
 	require __dir__.'/resources/bootstrap/bootstrap4_js.php';
-	
 	?>
 	<script type="text/javascript" src='resources/js/custom_JS_functions.js'></script>
 </body>
